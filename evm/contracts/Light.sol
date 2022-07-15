48d63f89e5af59a72750ee76c6b756d09a121dd5
Joshua Martin <josh.martin.hhsib@gmail.com>
2022-07-15 10:07:17 -0600

    Make contract an NFT

--- evm/contracts/Light.sol
+++ evm/contracts/Light.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Light {
-  constructor() public {
+contract Light is ERC721 {
+  constructor() ERC721("Light", "LT") {
   }
 }

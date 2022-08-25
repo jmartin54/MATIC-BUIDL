5a0165570b2c9b691d51397e8fbb16e3c1f4b4de
Joshua Martin <josh.martin.hhsib@gmail.com>
2022-08-24 23:43:46 -0600

    Add color form to sequence-integration

--- sequence-integration/src/Pixel.tsx
+++ sequence-integration/src/Pixel.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { sequence } from "0xsequence";
 import { ethers } from "ethers";
 import lightBuild from "./Light.json";
@@ -9,12 +9,21 @@ type LightInfo = {
   color: string;
 };
 
+enum ViewStates {
+  loading,
+  show,
+  setForm,
+  mintForm,
+}
+
 type PixelProps = {
   at: [number, number];
 };
 
 export default function Pixel({ at: [x, y] }: PixelProps) {
   const [info, setInfo] = useState<LightInfo | null>(null);
+  const [viewState, setViewState] = useState<ViewStates>(ViewStates.loading);
+  const colorInput = useRef<HTMLInputElement | null>(null);
 
   useEffect(() => {
     loadInfo();
@@ -45,10 +54,16 @@ export default function Pixel({ at: [x, y] }: PixelProps) {
         color: color,
         owner: info.owner,
       });
+
+      setViewState(ViewStates.show);
     })();
   }, []);
 
   const mint = useCallback(() => {
+    const colorValue = colorInput.current?.value;
+    const sanitized = `${colorValue}`.replace("#", "0x");
+    const color = parseInt(sanitized, 16);
+
     (async () => {
       // Get the wallet signer interface
       const wallet = sequence.getWallet();
@@ -64,12 +79,16 @@ export default function Pixel({ at: [x, y] }: PixelProps) {
 
       const connectedAddress = await wallet.getAddress();
 
-      await lightContract.mint(x, y, 0, 0x18f25669, connectedAddress);
+      await lightContract.mint(x, y, 0, color, connectedAddress);
       loadInfo();
     })();
   }, []);
 
   const set = useCallback(() => {
+    const colorValue = colorInput.current?.value;
+    const sanitized = `${colorValue}`.replace("#", "0x");
+    const color = parseInt(sanitized, 16);
+
     (async () => {
       // Get the wallet signer interface
       const wallet = sequence.getWallet();
@@ -83,36 +102,51 @@ export default function Pixel({ at: [x, y] }: PixelProps) {
         signer
       );
 
-      await lightContract.setColor(x, y, 0, 0x00ffff69);
+      await lightContract.setColor(x, y, 0, color);
       loadInfo();
     })();
   }, []);
 
   const handleClick = useCallback(() => {
-    console.log(x, y, info);
     if (info == null) return;
-    if (info.minted) set();
-    if (!info.minted) mint();
+    if (!info.minted) setViewState(ViewStates.mintForm);
+    if (info.minted) setViewState(ViewStates.setForm);
   }, [info]);
   return (
     <div
       style={{
-        margin: "0.5em",
+        margin: "0.0em",
         width: "7em",
         height: "7em",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
-        border: "1px solid #333",
+        //border: info?.minted ? `1px solid ${info?.color}` : "1px solid #333",
         backgroundColor: info?.color,
       }}
       onClick={handleClick}
     >
-      {info == null && <p style={{ margin: 0, padding: 0 }}>loading...</p>}
-      <h1 style={{ margin: 0, padding: 0 }}>
-        {x} x {y}
-      </h1>
+      {ViewStates.loading == viewState && (
+        <p style={{ margin: 0, padding: 0 }}>loading...</p>
+      )}
+      {ViewStates.show == viewState && (
+        <h1 style={{ margin: 0, padding: 0 }}>
+          {x} x {y}
+        </h1>
+      )}
+      {ViewStates.mintForm == viewState && (
+        <>
+          <input ref={colorInput} type="color" />
+          <button onClick={mint}>mint</button>
+        </>
+      )}
+      {ViewStates.setForm == viewState && (
+        <>
+          <input ref={colorInput} type="color" />
+          <button onClick={set}>set color</button>
+        </>
+      )}
     </div>
   );
 }
